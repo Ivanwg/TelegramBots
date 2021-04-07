1844fa911b074c00394cddb9c9cf17bf993722d9
Sevastyan Rakhimov <Rakhimov.SE@gmail.com>
2021-04-07 09:54:21 +0300

    Настроил работу с Dialogflow

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+from google.cloud import dialogflow
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/rakhimovse/Projects/tutor/Иван/TelegramBots/credentials.json'
+project_id = 'small-talk-jcctyj'
+session_id = '1'
+language_code = 'ru'
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    """Returns the result of detect intent with texts as inputs.
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+
+    for text in texts:
+        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+        query_input = dialogflow.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
+        )
+
+        print("=" * 20)
+        print("Query text: {}".format(response.query_result.query_text))
+        print(
+            "Detected intent: {} (confidence: {})\n".format(
+                response.query_result.intent.display_name,
+                response.query_result.intent_detection_confidence,
+            )
+        )
+        print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+
+
+text = input()
+detect_intent_texts(project_id, session_id, [text], 'ru')
