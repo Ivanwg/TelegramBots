4b2b58f618e9ea3018f936575f60ab2f659f94bc
Sevastyan Rakhimov <Rakhimov.SE@gmail.com>
2021-04-07 10:52:27 +0300

    Подключил configparser, поменял функцию detect_intent_texts

---
+++ dialogflow.py
@@ -0,0 +1,44 @@
+from google.cloud import dialogflow
+from configparser import ConfigParser
+import os
+
+config = ConfigParser()
+config.read('config.ini')
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = config['DIALOGFLOW']['credentials']
+project_id = config['DIALOGFLOW']['project_id']
+language_code = config['DIALOGFLOW']['language_code']
+
+
+def detect_intent_text(session_id, text):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    print("=" * 20)
+    print("Query text: {}".format(response.query_result.query_text))
+    print(
+        "Detected intent: {} (confidence: {})\n".format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence,
+        )
+    )
+    print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+
+
+text = input()
+detect_intent_text('1', text)
